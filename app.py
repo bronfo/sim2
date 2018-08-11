53e71ba8afe11cdb788248555321b5ff193cd5a9
test1 <test1@test1.com>
2018-08-11 15:44:56 +0800

    1

--- app.py
+++ app.py
@@ -1,154 +1,193 @@
 #coding:utf-8
-#import SimpleHTTPServer
-try:
-    from socketserver import ThreadingMixIn
-except ImportError:
-    from SocketServer import ThreadingMixIn
-try:
-    from http.server import BaseHTTPRequestHandler
-    from http.server import HTTPServer
-except ImportError:
-    from BaseHTTPServer import BaseHTTPRequestHandler
-    from BaseHTTPServer import HTTPServer
-import socket
-try:
-    from urllib.parse import urlparse
-except ImportError:
-    from urlparse import urlparse
-try:
-    from http.client import HTTPConnection
-except ImportError:
-    from httplib import HTTPConnection
-import os
+
 import logging
+import asyncio
+import websockets
 
-PORT = 8080
-RDBUFSZ = 10240
-
-g_id = 0
-g_connects = {}
-
-logger = logging.getLogger(os.path.basename(__file__))
-
-
-def crypt_string(data, encode=True, key='bjisa8g9kx7d8sskdg898d_xvc87d*84@*&x'):
-    from itertools import izip, cycle
-    import base64
-    if not encode:
-        data = base64.decodestring(data)
-    xored = ''.join(chr(ord(x) ^ ord(y)) for (x,y) in izip(data, cycle(key)))
-    if encode:
-        return base64.encodestring(xored).strip()
-    return xored
-
-class MyHandler(BaseHTTPRequestHandler):
-    #protocol_version = 'HTTP/1.1'
-    # todo id -- peer ip:port
-    # todo clean g_connects, and make threading-safety
-    def log_message(self, format, *args):
-        return
-    
-    def set_header(self, status, len):
-        self.send_response(status)
-        #self.send_header("Connection", "keep-alive")
-        self.send_header("Content-Length", str(len))
-        self.end_headers()
-    
-    def do_GET(self):
-        global g_id
-        global g_connects
-        if self.path.startswith("/connect/"):
-            try:
-                host, port = crypt_string(self.path[9:], False).split(":")
-                conn_to_target = socket.create_connection((host, int(port)))
-            except Exception as ex:
-                self.send_response(503)
-                self.end_headers()
-                return
-            g_id += 1
-            this_id = g_id
-            g_connects[str(this_id)] = {'conn': conn_to_target, 'addr': self.client_address}
-            data = 'hello %d' % this_id
-            self.set_header(200, len(data))
-            self.wfile.write(data)
-        elif self.path.startswith("/fetch/"):
-            id_str = self.path[7:]
-            #logger.info(id_str + " fetching")
-            data = ""
-            try:
-                data = g_connects[id_str]['conn'].recv(RDBUFSZ)
-            except Exception as ex1:
-                logger.debug("\t%s read from target fail" % id_str)
-            try:
-                if data:
-                    data = crypt_string(data)
-                    self.set_header(200, len(data))
-                    self.wfile.write(data)
-                    logger.debug("\t%s %s fetch [%d]" % (id_str, self.client_address, len(data)))
-                else:
-                    self.set_header(200, 0)
-                self.wfile.flush()
-            except Exception as ex2:
-                self.wfile._wbuf = [] # make finish() happy to flush it
-                logger.debug("write to client fail, clean it")
-        elif self.path.startswith("/get/"):
-            url = crypt_string(self.path[5:], False)
-            p = urlparse(url)
-            addr = p.netloc.split(":")
-            if len(addr) > 1:
-                host, port = addr
-            else:
-                host = addr[0]
-                port = 80
-            conn = HTTPConnection(host, int(port))
-            conn.request("GET", p.path + "?" + p.query)
-            resp = conn.getresponse()
-            data = resp.read()
-            cnt = str(len(data))
-            self.send_response(resp.status)
-            for k, v in resp.getheaders():
-                if k.lower() == "content-length":
-                    if cnt != v:
-                        logger.info("len: %s/%s" % (v, cnt))
-                    self.send_header(k, v)
-                elif k.lower() == "transfer-encoding" and v.lower() == "chunked":
-                    self.send_header("Content-Length", cnt)
-                else:
-                    self.send_header(k, v)
-            self.end_headers()
-            if data:
-                self.wfile.write(data)
-        elif self.path == "/":
-            data = "test success 15"
-            self.set_header(200, len(data))
-            self.wfile.write(data)
-        else:
-            self.send_response(403)
-
-    def do_POST(self):
-        if self.path.startswith("/post/"):
-            id_str = self.path[6:]
-            content_len = int(self.headers.getheader('content-length', 0))
-            if content_len > 0:
-                content = self.rfile.read(content_len)
-                logger.info("\t%s %s post [%d/%d]" % (id_str, self.client_address, content_len, len(content)))
-                g_connects[id_str]['conn'].sendall(crypt_string(content, False))
-            else:
-                logger.info(id_str + " post [0]?")
-            self.set_header(200, 2)
-            self.wfile.write('ok')
-        else:
-            self.set_header(404, 0)
-
-Handler = MyHandler
+from itertools import cycle
+import base64
 
 logging.basicConfig(format='%(asctime)s %(filename)s %(lineno)s: %(message)s')
-logger.setLevel(logging.DEBUG)
 
-class MyHTTPServer(ThreadingMixIn, HTTPServer):
-    pass
+BUFSIZE = 8196
 
-httpd = MyHTTPServer(("", PORT), Handler)
+class MsgType:
+    Connect = 1
+    Connect_OK = 2
+    Connect_Failure = 3
+    Data = 4
+    Disconnect = 5
+    CloseTunnel = 6
+    
+    FlagToClose = 911
+    KEY = b'xky0810'
+    
+    def pack(type, stream_id, data):
+        d = bytes([type]) + stream_id + data
+        return MsgType.crypt(d, MsgType.KEY, True)
+    
+    def unpack(data):
+        data = MsgType.crypt(data, MsgType.KEY, False)
+        return data[0], data[1:5], data[5:]
+    
+    def setaddr(host, port):
+        return ('%s:%d' % (host, port)).encode()
+    
+    def getaddr(data):
+        try:
+            host, port = data.split(b':')
+            return host, int(port)
+        except Exception:
+            return None, None
+    
+    # bytes data
+    def crypt(data, key, encode=True):
+        # python2 has izip, python3 has zip
+        izip = zip
+        # to bytes
+        #data = data.encode()
+        if not encode:
+            data = base64.b64decode(data)
+        #xored = ''.join(chr(ord(x) ^ ord(y)) for (x,y) in izip(data, cycle(key)))
+        xored = b''.join(bytes([x ^ y]) for (x,y) in izip(data, cycle(key)))
+        return base64.b64encode(xored) if encode else xored
 
-logger.info("serving v3 at port %d" % PORT)
-httpd.serve_forever()
+
+class Stream:
+    def __init__(self, server, stream_id, data):
+        self.server = server
+        self.stream_id = stream_id
+        self.data = data
+        self.write_queue = asyncio.Queue()
+    
+    async def run(self):
+        try:
+            self.host, self.port = MsgType.getaddr(self.data)
+            self.reader, self.writer = await asyncio.open_connection(
+                self.host, self.port)
+        except Exception:
+            logging.error('connect websocket with error ==>', exc_info=True)
+            msg = MsgType.pack(MsgType.Connect_Failure,
+                self.stream_id, b'')
+            await self.server.send_queue.put(msg)
+            del self.server.stream_map[self.stream_id]
+            return
+        
+        logging.error('connect %s:%d OK' % (self.host, self.port))
+        
+        msg = MsgType.pack(MsgType.Connect_OK, self.stream_id, b'')
+        await self.server.send_queue.put(msg)
+        
+        await asyncio.wait([self.read_from_remote(), self.write_to_remote()])
+    
+    async def read_from_remote(self):
+        while True:
+            try:
+                data = await self.reader.read(BUFSIZE)
+            except Exception:
+                logging.error('read from remote with ex ==>', exc_info=True)
+                data = b''
+            
+            logging.error('read [%s] from remote' % data[:16])
+            
+            if data:
+                msg = MsgType.pack(MsgType.Data, self.stream_id, data)
+                await self.server.send_queue.put(msg)
+            else:
+                msg = MsgType.pack(MsgType.Disconnect, self.stream_id, b'')
+                await self.server.send_queue.put(msg)
+                return
+    
+    
+    async def write_to_remote(self):
+        while True:
+            msg = await self.write_queue.get()
+            logging.error('writing [%s] to remote' %
+                (msg[:16] if type(msg)==bytes else msg))
+            if msg == MsgType.FlagToClose:
+                self.do_close()
+                return
+            
+            try:
+                self.writer.write(msg)
+                await self.writer.drain()
+            except Exception:
+                self.do_close()
+                return
+    
+    def do_close(self):
+        if not self.writer.transport.is_closing():
+            self.writer.write_eof()
+        if self.stream_id in self.server.stream_map:
+            del self.server.stream_map[self.stream_id]
+
+class Server:
+    def __init__(self, ws):
+        self.ws = ws
+        self.stream_map = {}
+        self.send_queue = asyncio.Queue()
+    
+    async def run(self):
+        logging.error('new websocket connection, one Server, startup send_to_peer, reading...')
+        send_task = asyncio.ensure_future(self.send_to_peer())
+        await self.read_from_peer()
+        logging.error('websocket read end')
+        send_task.cancel()
+    
+    async def send_to_peer(self):
+        while True:
+            data = await self.send_queue.get()
+            logging.error('queue [%s], sending to websocket' % data[:16])
+            try:
+                await self.ws.send(data)
+            except Exception:
+                logging.error('sending to websocket with error ==>', exc_info=True)
+                return
+
+    async def read_from_peer(self):
+        while True:
+            try:
+                data = await self.ws.recv()
+                logging.error('read from websocket: %s' % data[:16])
+            except Exception:
+                logging.error('read from websocket with error ==>', exc_info=True)
+                return
+            
+            type, stream_id, data = MsgType.unpack(data)
+            stm = 'found stream' if stream_id in self.stream_map else 'nofound stream'
+            logging.error('stream=%s' % stm)
+            
+            if type == MsgType.Connect:
+                logging.error('read Connect request')
+                stream = Stream(self, stream_id, data)
+                self.stream_map[stream_id] = stream
+                asyncio.ensure_future(stream.run())
+            
+            elif type == MsgType.Disconnect:
+                if stream_id in self.stream_map:
+                    await self.stream_map[stream_id
+                        ].write_queue.put(MsgType.FlagToClose)
+            elif type == MsgType.Data:
+                if stream_id in self.stream_map:
+                    await self.stream_map[stream_id
+                        ].write_queue.put(data)
+            else:
+                logging.error('websocket get unexpected msg')
+
+
+async def websocket_handle(ws, path):
+    server = Server(ws)
+    try:
+        await server.run()
+        logging.error('tunnel closed')
+    except Exception:
+        logging.error('server.run exception')
+
+async def main():
+    server = await websockets.serve(websocket_handle, '', 8080)
+    await server.wait_closed()
+
+
+if __name__ == '__main__':
+    logging.disable(logging.WARNING)
+    asyncio.get_event_loop().run_until_complete(main())
